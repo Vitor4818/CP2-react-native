8a4f2be90710ad5c98cdc590c977f1481c9c7939
30lima <pedrolimaa354@gmail.com>
2025-04-26 21:36:21 -0300

    feat: adicionando conteúdo da HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,25 @@
-import {Text} from "react-native"
-export default function HomeScreen(){
-    return(
-        <Text>Aqui é a Home</Text> 
-       )
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function HomeScreen({ navigation }: any) {
+    return (
+        <View style={styles.container}>
+            <Text>Home Screen</Text>
+            <Button
+                title="Perfil"
+                onPress={() => navigation.navigate('Profile')}
+            />
+            <Button
+                title="Detalhes"
+                onPress={() => navigation.navigate('Details')}
+            />
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
